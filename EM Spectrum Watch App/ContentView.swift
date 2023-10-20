bc2cbc5387aadbe19a292a02148becf84a960f8e
Joshua Peisach <itzswirlz2020@outlook.com>
2023-10-20 16:04:43 -0400

    Initial Commit

---
+++ EM Spectrum Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EM Spectrum Watch App
+//
+//  Created by Joshua Peisach on 10/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EM Spectrum Watch App/EM_SpectrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  EM_SpectrumApp.swift
+//  EM Spectrum Watch App
+//
+//  Created by Joshua Peisach on 10/20/23.
+//
+
+import SwiftUI
+
+@main
+struct EM_Spectrum_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
